7d82555e9ffaa0cd74c8b4b8cc2395d5e8e45a90
Anup Puri <admin@cdanup.com>
2017-11-10 01:24:15 -0500

    Add files via upload

---
+++ checkParity64BitWord/testCode/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace checkParity64BitWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Check the parity of a 64 bit integer");
+
+            //Random rnd = new Random();
+            //int val1 = rnd.Next(), val2 = rnd.Next();
+            long val = long.MaxValue;
+            Console.WriteLine(Convert.ToString(val, 2));
+
+            val ^= val >> 32;
+            val ^= val >> 16;
+            val ^= val >> 8;
+            val ^= val >> 4;
+            val ^= val >> 2;
+            val ^= val >> 1;
+
+            Console.WriteLine("Result = " + (val & 0x1).ToString());
+        }
+
+    }
+}
---
+++ checkTreeIsHeightBalanced/checkTreeIsHeightBalanced/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace checkTreeIsHeightBalanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            binaryTree tree = new binaryTree();
+            tree.add(17);
+            tree.add(10);
+            tree.add(24);
+            tree.add(12);
+            tree.add(9);
+            tree.add(18);
+            tree.add(25);
+
+            bool result = checkTreeIsHeightBalanced(tree.top);
+
+            Console.WriteLine();
+
+            if (result)
+                Console.WriteLine("Tree is height balanced");
+            else
+                Console.WriteLine("Tree is not height balanced");
+        }
+
+        private static bool checkTreeIsHeightBalanced(Node top)
+        {
+            int result = isBalanced(top);
+
+            if (result > 0)
+                return true;
+            else
+                return false;
+        }
+
+        private static int isBalanced(Node top)
+        {
+            if (top == null)
+                return 0;
+
+            int leftH = isBalanced(top.left);
+            if (leftH == -1)
+                return -1;
+
+            int rightH = isBalanced(top.right);
+            if (rightH == -1)
+                return -1;
+
+            if (Math.Abs(leftH - rightH) > 1)
+                return -1;
+
+            return 1 + Math.Max(leftH, rightH);
+        }
+    }
+
+    class binaryTree
+    {
+        public Node top;
+
+        public void add(int value)
+        {
+            if(top==null)
+                top = new Node(value);
+            else
+                top.add(top, value);
+        }
+
+        public bool contains(int value)
+        {
+            return top.contains(top, value);
+        }
+
+        public void printInOrder()
+        {
+            top.printInOrder(top);
+        }
+    }
+
+    class Node
+    {
+        public int data;
+        public Node left;
+        public Node right;
+
+        public Node(int value)
+        {
+            this.data = value;
+            this.left = null;
+            this.right = null;
+        }
+
+        internal void add(Node top, int value)
+        {
+            if (top == null)
+                top = new Node(value);
+            else
+            {
+                if (value < top.data)
+                    if (top.left == null)
+                        top.left = new Node(value);
+                    else
+                        top.left.add(top.left, value);
+                else
+                    if (top.right == null)
+                        top.right = new Node(value);
+                    else
+                        top.right.add(top.right, value);
+            }
+        }
+
+        internal bool contains(Node top, int value)
+        {
+            if (top.data == value)
+                return true;
+            else if (value < top.data)
+                return contains(top.left, value);
+            else
+                return contains(top.right, value);
+        }
+
+        internal void printInOrder(Node top)
+        {
+            if (top == null)
+                return;
+            else
+            {
+                if(top.left!=null)
+                    top.left.printInOrder(top.left);
+                
+                Console.Write(top.data + " ");
+
+                if(top.right!=null)
+                    top.left.printInOrder(top.right);
+            }
+        }
+    }
+}
