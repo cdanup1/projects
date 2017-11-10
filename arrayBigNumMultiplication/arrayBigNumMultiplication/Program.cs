f06dad95d0440080cb27e11fafaf6e2310e4d2ca
Anup Puri <admin@cdanup.com>
2017-11-10 01:15:36 -0500

    Add files via upload

---
+++ arrayBigNumMultiplication/arrayBigNumMultiplication/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrayBigNumMultiplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 2, 3, 4, 5, 6, 7 };
+            int[] b = { 7, 6, 5, 4, 3, 2, 0 };
+
+            int[] result = new int[a.Length + b.Length];
+
+            for (int i = a.Length - 1; i >= 0; --i)
+            {
+                for (int j = b.Length - 1; j >= 0; --j)
+                {
+                    result[i + j + 1] += a[i] * b[j];
+                    result[i + j] += (result[i + j + 1] / 10);
+                    result[i + j + 1] %= 10;
+                }
+            }
+
+            // Remove leading zeros
+            int nZeros = 0;
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (result[i] == 0)
+                {
+                    nZeros++;
+                }
+                else
+                    break;
+            }
+
+            int[] multi = new int[result.Length - nZeros];
+            Array.Copy(result, nZeros, multi, 0, result.Length - nZeros);
+            
+            //display
+            for (int i = 0; i < multi.Length; i++)
+            {
+                Console.Write(multi[i] + " ");
+            }
+        }
+    }
+}
---
+++ arrayNumAddLSB/arrayNumAddLSB/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrayNumAddLSB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = {2,1,9};
+
+            a[a.Length -1]++;
+            for (int i = a.Length-1; i >= 0; --i)
+            {
+                if (i == 0 && a[i] == 10)
+                {
+                    a[0] = 0;
+                    Array.Resize<int>(ref a, a.Length + 1);
+                    Array.Copy(a, 0, a, 1, a.Length - 2);
+                    a[0] = 1;
+                }
+
+                if (a[i] == 10)
+                {
+                    a[i] = 0;
+                    a[i - 1]++;
+                }
+            }
+
+            for (int i = 0; i < a.Length; i++)
+                Console.Write(a[i].ToString() + " ");
+        }
+    }
+}
